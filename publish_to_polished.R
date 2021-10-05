31c045dbae7693c9b7704050ed5580db91ab9f49
Jeremy Boyd <jeremyboyd@pm.me>
2021-10-05 16:31:21 -0600

    Revise notes.

--- publish_to_polished.R
+++ publish_to_polished.R
@@ -1,6 +1,10 @@
-# Code to deploy an app to Polished
-# App must be split into global, ui, and server files. App name is the name of
-# the directory.
+# Author: Jeremy Boyd (jeremyboyd@pm.me)
+# Description: Code to deploy an app to Polished.
+
+# NOTE: App must be split into global, ui, and server files. App name is the
+# name of the directory.
+# NOTE: Comment out the source() line in .Rprofile, move renv folder out of the
+# directory, and restart R. Otherwise the call to deploy_app() will fail.
 library(polished)
 polished::deploy_app(
     app_name = "MTB_results",
