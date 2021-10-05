cff198f3d108af86c8cda877520d96007c207f47
Jeremy Boyd <jeremyboyd@pm.me>
2021-10-05 16:30:23 -0600

    Revise to handle these date string formats that appeared in the McNabb 2021 data: H:MM:SS.SS, and MM:SS.SS.

--- to_minutes.R
+++ to_minutes.R
@@ -26,6 +26,20 @@ to_minutes <- Vectorize(function(time_string) {
         minutes <- (hour(time_period) * 60 * 60 + minute(time_period) * 60 +
                         second(time_period)) / 60 }
     
+    # For McNabb 2021, times are to the hundredth of a second
+    else if(str_detect(time_string, "^[0-9]:[0-9]{2}:[0-9]{2}\\.[0-9]{2}$")) {
+        time_period <- hms(time_string)
+        minutes <- (hour(time_period) * 60 * 60 + minute(time_period) * 60 +
+                        second(time_period)) / 60 }
+    
+    # For H:MM:SS.SS, need to add "0:" to the start of the string in order for
+    # hms() to work correctly.
+    else if(str_detect(time_string, "^[0-9]{2}:[0-9]{2}\\.[0-9]{2}$")) {
+        time_period <- hms(paste0("0:", time_string))
+        minutes <- (hour(time_period) * 60 * 60 + minute(time_period) * 60 +
+                        second(time_period)) / 60 
+    }
+    
     # Convert x representations and DNF to NA
     else if(str_detect(time_string, "^X X X X$|x:xx:xx|DNF"))
     { minutes <- NA_real_ }
